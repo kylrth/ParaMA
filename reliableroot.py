d274406053460a11f633fc4d7ed614b407408e41
kylrth <kylrth@gmail.com>
2018-12-15 13:28:11 -0700

    comments for reliableroot.py

--- reliableroot.py
+++ reliableroot.py
@@ -5,6 +5,11 @@ Created on Jun 11, 2018
 '''
 
 def is_reliable_root(root, freq):
+    """Determines whether a root is reliable given its frequency and length.
+    
+    Works on the idea that roots tend to have a minimum length (referred to in section 4.1 paragraph 1 in the paper),
+    and that longer proposed roots are more likely to be real. Thus, we require less evidence to support their reality.
+    """
     root_len = len(root)
     if root_len < 3: return False
     if root_len == 3 and freq < 2000: return False
