b7ae65eb39c1f9e7dbf9889b82a74a61612dca9c
kylrth <kylrth@gmail.com>
2018-12-15 13:33:31 -0700

    comments for segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -6,6 +6,18 @@ Created on Jun 11, 2018
 
 
 def get_seg_dict_by_token_dict(token_seg_dict):
+    """Converts from: a mapping from tokens to their immediate segmentation
+                  to: a mapping from tokens to their component subtokens, with hierarchical combination information.
+
+    Silly example:
+        [('theon', ('the', 'on', 'the', '$'))]
+            ->
+        [
+            ('the', (('the',), (('the', '$', '$'),))),
+            ('on', (('on',), (('on', '$', '$'),))),
+            ('theon', (('the', 'on'), (('the', '$', '$'), ('the', '$', 'on'))))
+        ]
+    """
     seg_dict = {}
     for word in token_seg_dict.keys():
         if word in seg_dict: continue
@@ -16,18 +28,19 @@ def get_seg_dict_by_token_dict(token_seg_dict):
                 wd_stack.pop()
                 continue
             if wd in token_seg_dict:
-                #morph, suffix, root, trans
+                # get morph, suffix, root, trans for this transformation
                 wd_morph, wd_suffix, wd_root, wd_trans = token_seg_dict[wd]
-                if wd_suffix == '$' or wd_suffix == '':
+                if wd_suffix == '$' or wd_suffix == '':  # if we've reached the bottom root
                     seg_dict[wd] = ((wd_morph,), ((wd_root, wd_trans, wd_suffix),))
                     wd_stack.pop()
                     continue
                 elif wd_root in seg_dict:
-                    #examples: loneliness = loneli (lonely -y+i) +ness;    lonely = lone () +ly
-                    #Alignment:
-                    #--REP: lone+ly -> lone+li
-                    #--DEL: en+force -> en+forc
-                    #--DUP: under+pin -> under+pinn
+                    # The root is morphologically complex, and we already know the breakdown.
+                    # examples: loneliness = loneli (lonely -y+i) +ness;    lonely = lone () +ly
+                    # Alignment:
+                    # --REP: lone+ly -> lone+li
+                    # --DEL: en+force -> en+forc
+                    # --DUP: under+pin -> under+pinn
                     rt_seg = seg_dict[wd_root]
                     rt_morphs = rt_seg[0]
                     morphs = []
@@ -44,17 +57,19 @@ def get_seg_dict_by_token_dict(token_seg_dict):
                     wd_stack.pop()
                     continue
                 else:
-                    wd_stack.append(wd_root)
+                    # The root is morphologically complex, but we don't have the breakdown yet.
+                    wd_stack.append(wd_root)  # add it to the stack of words to handle
                     continue
             else:
-                #inferenced non-appeared root:
-                #example: communicating = communicat + ing
+                # inferred non-appearing root
+                # example: communicating = communicat + ing
                 seg_dict[wd] = ((wd,), ((wd, '$', '$'),))
                 wd_stack.pop()
                 continue
     return seg_dict
 
 def seg_dict_update(seg_dict):
+    """This function appears to split up morphologically complex roots further. It's not used anywhere."""
     #Recursively update segmentation if root is segmented
     updated_seg = {}
     for word, (morphs, components) in seg_dict.items():
@@ -83,10 +98,13 @@ def seg_dict_update(seg_dict):
     seg_dict.update(updated_seg)
 
 def get_seg_dict_by_paradigms(paradigm_dict):
+    """Given paradigms, create a mapping from tokens to their segmentation info."""
+    # create a mapping from tokens to the transformation that creates them
     token_seg_dict = {}
     for root, word_list in paradigm_dict.items():
         for word, trans, suffix, stem in word_list:
             token_seg_dict[word] = (stem, suffix, root, trans)
+    # convert to a mapping from tokens to their segmentation info in a hierarchical structure
     seg_dict = get_seg_dict_by_token_dict(token_seg_dict)
     return seg_dict
 
